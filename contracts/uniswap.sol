ac5ff8b2a46e9ce14e0e35d24fc25769e058584a
singh6678 <aryendra@genesisblockchain.io>
2023-01-22 00:37:46 +0530

    first commit

---
+++ contracts/uniswap.sol
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import "./tokenB.sol";
+
+contract TestUniswapLiquidity {
+    address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+    address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    Token tokenB;
+
+    constructor(address token){
+        tokenB = Token(token);
+    }
+
+    function addLiquidity(
+        address _tokenA,
+        address _tokenB,
+        uint _amountA,
+        uint _amountB
+    ) external {
+        IERC20(_tokenA).transferFrom(msg.sender, address(this), _amountA);
+        IERC20(_tokenB).transferFrom(msg.sender, address(this), _amountB);
+        uint amount_b1 = tokenB.balancefee(_amountB);
+        IERC20(_tokenA).approve(ROUTER, _amountA);
+        IERC20(_tokenB).approve(ROUTER, amount_b1);
+        uint amount_b2 = tokenB.balancefee(amount_b1);
+
+        (uint amountA, uint amountB, uint liquidity) = IUniswapV2Router(ROUTER)
+            .addLiquidity(
+                _tokenA,
+                _tokenB,
+                _amountA,
+                amount_b2,
+                0,
+                0,
+                msg.sender,
+                block.timestamp+360000
+            );
+    }
+
+    function removeLiquidity(address _tokenA, address _tokenB) external {
+        address pair = IUniswapV2Factory(FACTORY).getPair(_tokenA, _tokenB);
+
+        uint liquidity = IERC20(pair).balanceOf(address(this));
+        IERC20(pair).approve(ROUTER, liquidity);
+
+        (uint amountA, uint amountB) = IUniswapV2Router(ROUTER).removeLiquidity(
+            _tokenA,
+            _tokenB,
+            liquidity,
+            1,
+            1,
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function swap(
+    address _tokenIn,
+    address _tokenOut,
+    uint _amountIn,
+    uint _amountOutMin,
+    address _to
+  ) external {
+    IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+    uint amount_b1 = tokenB.balancefee(_amountIn);
+    IERC20(_tokenIn).approve(ROUTER, amount_b1);
+    
+
+    address[] memory path;
+    if (_tokenIn == WETH || _tokenOut == WETH) {
+      path = new address[](2);
+      path[0] = _tokenIn;
+      path[1] = _tokenOut;
+    } else {
+      path = new address[](2);
+      path[0] = _tokenIn;
+      //path[1] = WETH;
+      path[1] = _tokenOut;
+    }
+
+    IUniswapV2Router(ROUTER).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+      amount_b1,
+      _amountOutMin,
+      path,
+      _to,
+      block.timestamp+36000
+    );
+  }
+
+  
+}
+
+interface IUniswapV2Router {
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    )
+        external
+        returns (
+            uint amountA,
+            uint amountB,
+            uint liquidity
+        );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external;
+}
+
+interface IUniswapV2Factory {
+    function getPair(address token0, address token1) external view returns (address);
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+}
